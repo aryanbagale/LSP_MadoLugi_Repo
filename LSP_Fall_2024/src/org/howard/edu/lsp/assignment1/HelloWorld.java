258738db546e41166cc71cebde02f55e109850b2
aryan <aryan@10.0.0.91>
2024-09-12 22:45:26 -0400

    First commit of Assignment 1

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Name: Aryan Bagale
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
